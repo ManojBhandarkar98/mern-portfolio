cb39da79b017603a70c34d5c7ec83238e872d58c
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-04-05 13:48:10 +0530

    portfolio - message controllers

---
+++ controllers/messageController.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ErrorHandler from '../middlewares/error'
+import { catchAsyncErrors } from '../middlewares/catchAsyncErrors'
+
+const messageController = () => {
+  return (
+    <div>messageController</div>
+  )
+}
+
+export default messageController
