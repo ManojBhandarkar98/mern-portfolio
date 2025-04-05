2f84da62a3b026b8c98d74d79c1e22db6f7fc735
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-04-05 13:43:12 +0530

    portfolio - server file - cloudinary setup done and cors done

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import dbConnection from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 const app = express();
 
+dotenv.config({ path: "./config/config.env" });
 
+app.use(
+  cors({
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
