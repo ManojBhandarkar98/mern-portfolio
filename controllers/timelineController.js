b007c9f4cdfb7d70f3f4c6089c4dde1fa4147f7b
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-04-12 17:50:16 +0530

    portfolio - timelineController - posttimeline

---
+++ controllers/timelineController.js
@@ -0,0 +1,17 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/error.js";
+import { Timeline } from "../models/timelineSchema.js";
+
+export const postTimeline = catchAsyncErrors(async (req, res, next) => {
+  const { title, description, from, to } = req.body;
+  const newTimeline = await Timeline.create({
+    title,
+    description,
+    timeline: { from, to },
+  });
+  res.status(200).json({
+    success: true,
+    message: "Timeline Added!",
+    newTimeline,
+  });
+});
